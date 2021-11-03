4683f6a37eda63053546257224ae31efd9208b23
Deniz Silahcilar <deniz.silahcilar@ing.com>
2021-11-03 12:44:46 +0100

    add lab2 solutions

---
+++ src/main/kotlin/com/ing/springintegration/ConsumerFlowConfiguration.kt
@@ -0,0 +1,46 @@
+package com.ing.springintegration
+
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.integration.channel.DirectChannel
+import org.springframework.integration.dsl.IntegrationFlow
+import org.springframework.integration.dsl.IntegrationFlows
+import org.springframework.integration.dsl.MessageChannels
+import org.springframework.integration.jms.dsl.Jms
+import org.springframework.jms.connection.SingleConnectionFactory
+
+@Configuration
+class ConsumerFlowConfiguration {
+    companion object{
+        private var logger = LoggerFactory.getLogger(ConsumerFlowConfiguration::class.java)
+    }
+    @Bean
+    fun reqFlow(connectionFactory: SingleConnectionFactory): IntegrationFlow {
+        return IntegrationFlows
+            .from(reqChannel())
+            .log<String> {
+                logger.debug(it.payload)
+            }.handle(
+                Jms.outboundAdapter(
+                    connectionFactory
+                ).destination("reqq")
+            ).get()
+    }
+
+    @Bean
+    fun repFlow(connectionFactory: SingleConnectionFactory): IntegrationFlow {
+        return IntegrationFlows
+            .from(
+                Jms.messageDrivenChannelAdapter(connectionFactory)
+                    .destination("repq")
+            ).log<String> {
+                logger.debug(it.payload)
+            }.handle {
+                it-> logger.info("I handled with message: $it")
+            }.get()
+    }
+
+    @Bean
+    fun reqChannel() = MessageChannels.direct().get()
+}
--- src/main/kotlin/com/ing/springintegration/Producer.kt
+++ src/main/kotlin/com/ing/springintegration/Producer.kt
@@ -1,6 +1,8 @@
 package com.ing.springintegration
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.integration.channel.DirectChannel
+import org.springframework.integration.support.MessageBuilder
 import org.springframework.jms.core.JmsTemplate
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -11,19 +13,19 @@ import javax.jms.Queue
 @RestController
 class Producer {
     @Autowired
-    private lateinit var reqq: Queue
-
-    @Autowired
     private lateinit var repq: Queue
 
     @Autowired
     private lateinit var jmsTemplate: JmsTemplate
 
+    @Autowired
+    private lateinit var reqChannel: DirectChannel
+
     @GetMapping("req/{msg}")
     fun req(@PathVariable("msg") msg: String): String {
         //TODO 1: Remove this part and use DSL to send message to req queue
         // dont wait for the response from reply queue
-        jmsTemplate.convertAndSend(reqq, msg)
+        reqChannel.send(MessageBuilder.withPayload(msg).build())
         return "Your message <b>$msg</b> published successfully"
     }
 
