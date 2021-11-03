c0db90011e2ad2060ab468c575c62f1454e2f71f
Deniz Silahcilar <deniz.silahcilar@ing.com>
2021-11-03 14:46:37 +0100

    add lab6 solutions

--- src/main/kotlin/com/ing/springintegration/ConsumerFlowConfiguration.kt
+++ src/main/kotlin/com/ing/springintegration/ConsumerFlowConfiguration.kt
@@ -3,6 +3,7 @@ package com.ing.springintegration
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.integration.core.MessagingTemplate
 import org.springframework.integration.dsl.IntegrationFlow
 import org.springframework.integration.dsl.IntegrationFlows
 import org.springframework.integration.dsl.MessageChannels
@@ -15,11 +16,6 @@ class ConsumerFlowConfiguration {
         private var logger = LoggerFactory.getLogger(ConsumerFlowConfiguration::class.java)
     }
 
-    //TODO 2 : run it and test it for multiple thread
-    // Execute multiple curl request from different tabs
-    // curl localhost/req/req-5
-    // curl localhost/req/req-1
-    // TODO 2 : fix the issue
     @Bean
     fun reqFlow(connectionFactory: SingleConnectionFactory): IntegrationFlow {
         return IntegrationFlows
@@ -34,7 +30,7 @@ class ConsumerFlowConfiguration {
                     .replyDestination("repq")
                     .correlationKey("JMSCorrelationID")
                     .receiveTimeout(25000)
-            ).channel(repChannel())
+            )
             .get()
     }
 
@@ -42,5 +38,5 @@ class ConsumerFlowConfiguration {
     fun reqChannel() = MessageChannels.direct().get()
 
     @Bean
-    fun repChannel() = MessageChannels.queue().get()
+    fun gatewayTemplate() = MessagingTemplate(reqChannel())
 }
--- src/main/kotlin/com/ing/springintegration/Producer.kt
+++ src/main/kotlin/com/ing/springintegration/Producer.kt
@@ -1,10 +1,8 @@
 package com.ing.springintegration
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.integration.channel.DirectChannel
+import org.springframework.integration.core.MessagingTemplate
 import org.springframework.integration.support.MessageBuilder
-import org.springframework.jms.core.JmsTemplate
-import org.springframework.messaging.PollableChannel
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -12,21 +10,11 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class Producer {
-    @Autowired
-    private lateinit var jmsTemplate: JmsTemplate
-
-    @Autowired
-    private lateinit var reqChannel: DirectChannel
-
-    @Autowired lateinit var repChannel: PollableChannel
+    @Autowired lateinit var gatewayTemplate: MessagingTemplate
 
     @GetMapping("req/{msg}")
     fun req(@PathVariable("msg") msg: String): String {
-        //TODO 1: Wait for the response from reply queue
-        reqChannel.send(MessageBuilder.withPayload(msg).build())
-        var waitMs = msg.split("-")[1].toLong() * 1000
-        Thread.sleep(waitMs)
-        var reply = repChannel.receive()
+        var reply = gatewayTemplate.sendAndReceive(MessageBuilder.withPayload(msg).build())
         return "Your reply is <b>$reply</b> published successfully"
     }
 
