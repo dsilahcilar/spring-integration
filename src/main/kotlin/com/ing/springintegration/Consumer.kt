542788b12d3e02a7fbb0c1abffe9f2b5a6aca6af
Deniz Silahcilar <deniz.silahcilar@ing.com>
2021-11-03 11:31:26 +0100

    lab1 question

--- src/main/kotlin/com/ing/springintegration/Consumer.kt
+++ src/main/kotlin/com/ing/springintegration/Consumer.kt
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Component
 
 @Component
 class Consumer {
+
+    //TODO: remove this part and
+    // consume from reqq and produce to repq
+    // DO it with spring integration DSL
     @JmsListener(destination = "reqq")
     fun reqListener(msg: String) {
         println("Request Received : $msg")
